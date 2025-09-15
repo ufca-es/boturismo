729f466eaa3a6b7fee2ebfe64c954ccab9ba7f81
cicero_jesus <cicero.jesus@aluno.ufca.edu.br>
2025-09-15 03:04:08 -0300

    Sugerir perguntas e mais saudacoes adicionadas ao bot

--- chatbot/loader.py
+++
@@ -1,9 +0,0 @@
-import json
-
-class ResponseLoader:
-    def __init__(self, filepath: str):
-        self.filepath = filepath
-    
-    def load_responses(self) -> dict:
-        with open(self.filepath, "r", encoding="utf-8") as file:
-            return json.load(file)
